1b3dea963b9798b24e15bff971c8fbba6767bd87
Dyshliuk Dmytro <koiiimap225@gmail.com>
2026-06-26 17:47:34 +0300

    fix: add rewrites to bypass CORS for MVP

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://staybase.software/:path*',
+      },
+    ];
   },
 };
 
